8e4c0b42e36b4b2f2813bc828cef8931d348b85c
kpgoing <xubowei96@gmail.com>
2018-09-09 15:37:37 +0800

    [feature] Add tools for transaction AOP.

--- src/main/java/club/xubowei/epframework/helper/AopHelper.java
+++ src/main/java/club/xubowei/epframework/helper/AopHelper.java
@@ -1,9 +1,11 @@
 package club.xubowei.epframework.helper;
 
 import club.xubowei.epframework.annotation.Aspect;
+import club.xubowei.epframework.annotation.Service;
 import club.xubowei.epframework.proxy.AbstractAspectProxy;
 import club.xubowei.epframework.proxy.Proxy;
 import club.xubowei.epframework.proxy.ProxyManager;
+import club.xubowei.epframework.proxy.TransactionProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,10 +40,12 @@ public final class AopHelper {
     private static Set<Class<?>> createTargetClassSet(Aspect aspect) {
 
         Class<? extends Annotation> annotationClass = aspect.value();
-        Set<Class<?>> targetClassSet = new HashSet<>();
+        Set<Class<?>> targetClassSet;
 
         if (!annotationClass.equals(Aspect.class)) {
             targetClassSet = ClassHelper.getClassSetByAnnotationPresent(annotationClass);
+        } else {
+            targetClassSet = new HashSet<>();
         }
 
         return targetClassSet;
@@ -52,12 +56,25 @@ public final class AopHelper {
      * Generate a mapping of [proxy class: proxy class (target class)]
      */
     private static Map<Class<?>, Set<Class<?>>> createProxyMap() {
+        Map<Class<?>, Set<Class<?>>> proxyMap = new HashMap<>();
 
-        return ClassHelper.getClassSetBySuper(AbstractAspectProxy.class)
-                .stream()
-                .filter((cls) -> cls.isAnnotationPresent(Aspect.class))
-                .collect(Collectors.toMap(cls -> cls, cls -> createTargetClassSet(cls.getAnnotation(Aspect.class))));
+        addAbstractAspectProxy(proxyMap);
+        addTransactionProxy(proxyMap);
 
+        return proxyMap;
+    }
+
+    private static void addAbstractAspectProxy(Map<Class<?>, Set<Class<?>>> proxyMap) {
+        proxyMap.putAll(
+                ClassHelper.getClassSetBySuper(AbstractAspectProxy.class)
+                        .stream()
+                        .filter((cls) -> cls.isAnnotationPresent(Aspect.class))
+                        .collect(Collectors.toMap(cls -> cls, cls -> createTargetClassSet(cls.getAnnotation(Aspect.class))))
+        );
+    }
+
+    private static void addTransactionProxy(Map<Class<?>, Set<Class<?>>> proxyMap) {
+        proxyMap.put(TransactionProxy.class, ClassHelper.getClassSetByAnnotationPresent(Service.class));
     }
 
     private static Map<Class<?>, List<Proxy>> createTargetMap(Map<Class<?>, Set<Class<?>>> proxyMap) throws Exception {
@@ -70,7 +87,7 @@ public final class AopHelper {
                 if (targetMap.containsKey(targetClass)) {
                     targetMap.get(targetClass).add(proxy);
                 } else {
-                    targetMap.put(targetClass, new ArrayList<>(Arrays.asList(proxy)));
+                    targetMap.put(targetClass, new ArrayList<>(Collections.singletonList(proxy)));
                 }
             }
         }
